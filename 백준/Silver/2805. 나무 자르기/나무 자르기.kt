473d37788e1a99db62ab8ed3929d88cecc8fdfd8
bjy557 <bjy557@naver.com>
2023-05-22 14:49:36 +0900

    [Silver II] Title: 나무 자르기, Time: 508 ms, Memory: 168116 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.kt
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.kt
@@ -1,20 +1,16 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.math.BigInteger
 import java.util.*
 import kotlin.math.max
 
-val arr = IntArray(1000000)
-var min = 0
-var n = 0
-var m = 0
-
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val st1 = StringTokenizer(readLine(), " ")
-    n = st1.nextToken().toInt()
-    m = st1.nextToken().toInt()
+    val n = st1.nextToken().toInt()
+    val m = st1.nextToken().toInt()
 
     var max = 0
+    var min = 0
+    val arr = IntArray(1000000)
 
     val st2 = StringTokenizer(readLine(), " ")
 
@@ -22,27 +18,28 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
         arr[i] = st2.nextToken().toInt()
         max = max(max, arr[i])
     }
-
-    findValue(max, 0)
     
-    print(min)
-}
-
-fun findValue(x: Int, y: Int) {
-    val half = (x + y) / 2
-    if (half == y) {
-        min = half
-    } else {
-        var tree = BigInteger("0")
+    while(min <= max) {
+        var half = (min + max) / 2
+        if (half == min) {
+            min = half
+            break
+        }
+        var tree = 0L
         for (i in 0 until n) {
-            if (arr[i] >= half) {
-                tree += (arr[i] - half).toBigInteger()
+            if (arr[i] > half) {
+                tree += (arr[i] - half)
             }
         }
         when {
-            tree == m.toBigInteger() -> min = half
-            tree < m.toBigInteger() -> findValue(half, y)
-            tree > m.toBigInteger() -> findValue(x, half)
+            tree == m.toLong() -> {
+                min = half
+                break
+            }
+            tree < m.toLong() -> max = half
+            tree > m.toLong() -> min = half
         }
     }
+    
+    print(min)
 }
